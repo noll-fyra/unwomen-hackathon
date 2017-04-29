6e42c1017f3821b0bf6683f214a5eb8fa46d18d2
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-30 06:10:17 +0800

    add stop path

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var isLoggedIn = require('../middleware/isLoggedIn')
 var Condition = require('../models/condition')
+var schedule = require('../utilities/schedule')
 // var User = require('../models/user')
 // var sendSms = require('../utilities/send-sms')
 
@@ -16,12 +17,12 @@ router.route('/')
     res.render('account/account', {data: data})
   })
 })
+// update contraceptive
 .post((req, res) => {
   if (req.body.contraceptiveType && req.body.contraceptiveBrand) {
     req.user.update({contraceptiveType: req.body.contraceptiveType, contraceptiveBrand: req.body.contraceptiveBrand}).exec((err, data) => {
       if (err) throw err
       res.redirect('/account')
-
     })
   } else {
     req.flash('error', 'You must enter both contraceptive fields.')
@@ -29,16 +30,28 @@ router.route('/')
   }
 })
 
+// start tracking
 router.route('/track')
 .post((req, res) => {
   if (req.body.track) {
     req.user.update({track: true}).exec((err, data) => {
       if (err) throw err
+      schedule.create(req.user)
       res.redirect('/account')
     })
   }
 })
 
-// <button onclick='<%- twilio(currentUser.phone).bind(this) %>' type="button" name="button">Send me a message</button>
+// stop tracking
+router.route('/stop')
+.post((req, res) => {
+  if (!req.body.track) {
+    req.user.update({track: false}).exec((err, data) => {
+      if (err) throw err
+      schedule.cancel(req.user)
+      res.redirect('/account')
+    })
+  }
+})
 
 module.exports = router
---
+++ utilities/schedule.js
@@ -0,0 +1,18 @@
+var schedule = require('node-schedule')
+var twilio = require('twilio')
+
+function create (user) {
+  var startTime = new Date()
+  var endTime = new Date(startTime.getTime() + 8 * 7 * 24 * 60 * 60 * 1000)
+  schedule.scheduleJob(user.id, {start: startTime, end: endTime, rule: '* * 18 * * *'}, () => {
+    twilio(user.number)
+  })
+}
+
+function cancel (user) {
+  var job = schedule.scheduledJobs[user.id]
+  job.cancel()
+}
+
+module.exports.create = create
+module.exports.cancel = cancel
