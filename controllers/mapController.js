1d96b879156b4c38ae36a1de76c9505d154c4e0c
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-29 16:15:15 +0800

    continue map testing

--- controllers/mapController.js
+++ controllers/mapController.js
@@ -4,18 +4,21 @@ var geocoder = require('geocoder')
 
 // the home page
 router.get('/', (req, res) => {
-    res.render('./mapForm')
+  res.render('./mapForm')
 })
 
 router.post('/', (req, res) => {
-  geocoder.geocode(req.body.address, (err, data) => {
-    if (err) throw err
-    // console.log(JSON.stringify(data))
-    res.render('./map', {map: data})
-  })
+  console.log(req.body)
+  if (req.body.address) {
+    geocoder.geocode(req.body.address, (err, data) => {
+      if (err) throw err
+      res.render('./map', {mapLat: data.results[0].geometry.location.lat, mapLong: data.results[0].geometry.location.lng})
+    })
+  } else if (req.body.yourlat && req.body.yourlong) {
+    res.render('./map', {mapLat: req.body.yourlat, mapLong: req.body.yourlong})
+  } else {
+    res.send('error finding location please go back and try again')
+  }
 })
-
-
-
 
 module.exports = router
