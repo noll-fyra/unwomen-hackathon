823bbb11b9f41a157413aae593ebc1338dbabaf0
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-30 01:48:40 +0800

    add condition over time

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var router = express.Router()
 var isLoggedIn = require('../middleware/isLoggedIn')
-var sendSms = require('../utilities/send-sms')
+var Condition = require('../models/condition')
+// var sendSms = require('../utilities/send-sms')
 
 // check that the user is logged in to access their account
 router.use(isLoggedIn)
@@ -9,9 +10,12 @@ router.use(isLoggedIn)
 // display the user's account
 router.route('/')
 .get((req, res) => {
-  res.render('account/account', {twilio: sendSms})
-//<button onclick='<%- twilio(currentUser.phone).bind(this) %>' type="button" name="button">Send me a message</button>
+  Condition.find({user: req.user}).sort({ date: 1 }).exec((err, data) => {
+    if (err) throw err
+    res.render('account/account', {data: data})
+  })
 
+// <button onclick='<%- twilio(currentUser.phone).bind(this) %>' type="button" name="button">Send me a message</button>
 })
 
 module.exports = router
