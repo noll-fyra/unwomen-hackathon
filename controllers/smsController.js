1bc507af024e5a3396d4b516df11209bada7de77
Jonathan Louis Ng <jonathanlouisng@gmail.com>
2017-04-29 23:36:00 +0800

    sms testing

--- controllers/smsController.js
+++ controllers/smsController.js
@@ -5,25 +5,30 @@ var User = require('../models/user')
 
 // the home page
 router.post('/', (req, res) => {
-  // console.log('the real one = req.body.Body and req.body.From')
-  // console.log(req.body.Body)
+  console.log('the phone number is')
+  console.log(req.body.From)
   // console.log('end')
   var twiml = new twilio.TwimlResponse()
 
 // 0 and 6 to allow only 1-5 responses
   if (!Number.isNaN(parseInt(req.body.Body)) && parseInt(req.body.Body) > 0 && parseInt(req.body.Body) < 6) {
-    User.find({phone: req.body.From}).exec((err, user) => {
-      if (err) {
-        twiml.message('Thanks for your interest in we-contraception, but your number does not appear to be listed. Visit http://we-contraception.herokuapp.com to find out more.')
-      } else {
-        twiml.message('Thanks ' + user.name + ', your response of ' + req.body.Body + ' has been recorded! Stay safe!')
-      }
-    })
+        twiml.message('Thanks, your response of ' + req.body.Body + ' has been recorded! Stay safe!')
+
+
   } else {
     twiml.message('Your entry is invalid. From a scale of 1 - 5, with 5 being the happiest, please tell us how happy you were today:')
   }
+
+  User.find({phone: req.body.From}).exec((err, user) => {
+    if (err) {
+      console.log('Thanks for your interest in we-contraception, but your number does not appear to be listed. Visit http://we-contraception.herokuapp.com to find out more.')
+    } else {
+      console.log('Thanks ' + user.name + ', your response of ' + req.body.Body + ' has been recorded! Stay safe!')
+    }
+  })
   res.writeHead(200, {'Content-Type': 'text/xml'})
   res.end(twiml.toString())
+
 })
 
 module.exports = router
